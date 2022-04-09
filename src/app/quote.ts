4be39e1aa61388ce711610071bcd9c75bdace10d
Dindihub <sadindi03@gmail.com>
2022-04-09 15:48:22 +0300

    Code-Add name parameter to class

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,10 +1,18 @@
+
+
 export class Quote {
-    constructor(public id:number,public quote:string,public source:any, public upvotes:number, public downvotes:number){
+    constructor(public id:number,public title:any ,public quote:string,public source:string, public datePosted:number,public upvotes:number, public downvotes:number, public popularQuotes:boolean){
         this.id=id;
+        this.title
         this.quote=quote;
         this.source=source;
+        this.datePosted=datePosted
+        this.popularQuotes=popularQuotes;
         this.upvotes=upvotes;
         this.downvotes=downvotes;
+
+    
+
 
     } 
     
