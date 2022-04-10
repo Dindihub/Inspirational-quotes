f470b29dd776626e4e1e639a5de45185a41e2756
Dindihub <sadindi03@gmail.com>
2022-04-10 20:16:46 +0300

    Code-Add code

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-quote-form',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
+  
+  @Output() addQuote= new EventEmitter();
+public name='';
+public userName='';
+public title='';
+public quote='';
+public source ='';
+public datePosted='';
+public upvotes='';
+public downvotes='';
+
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+ public addNewQuote(){
+const quote=new Quote(this.name,this.title,this.quote,this.source,new Date(),0,0); 
+ this.addQuote.emit(quote);
 }
+}
+
